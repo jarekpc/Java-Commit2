f6fc56f73fa6c90439281e2fd40e8306c7397280
jarekpc <jarekpc@o2.pl>
2016-08-10 21:03:20 +0200

    dodanie daty

--- src/com/jarek/App.java
+++ src/com/jarek/App.java
@@ -1,10 +1,13 @@
 package com.jarek;
 
+import java.util.Date;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("App");
+		Date data = new Date();
 	}
 
 }
