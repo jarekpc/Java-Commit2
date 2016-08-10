712dd4d5c43869dc1f357b7ed7337c7e1488e1ea
jarekpc <jarekpc@o2.pl>
2016-08-10 22:09:23 +0200

    remove line

--- src/com/jarek/App.java
+++ src/com/jarek/App.java
@@ -5,7 +5,6 @@ import java.util.Date;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("App");
 		Date data = new Date();
 		System.out.println("Hello!");
